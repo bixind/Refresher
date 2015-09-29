28216ae91dbcd593ba566cc518a5033a23c32a94
bixind <bixind@users.noreply.github.com>
2015-09-29 22:40:04 +0400

    Ref over!

--- main.py
+++ main.py
@@ -13,31 +13,6 @@ def configparse(name):
     f.close()
     return cnfg
 
-
-def update(curnses):
-    global last_message_id
-    try:
-        data = urlencode({'last_message_id' : last_message_id,
-                          'count' : 10,
-                          'access_token' : key,
-                          'v' : '5.37'
-                          })
-        w = urlopen("https://api.vk.com/method/messages.get?" + data)
-        response = json.loads(w.read().decode())['response']['items']
-    except:
-        return
-    if (response):
-        last_message_id = response[0]['id']
-        for msg in response:
-            try:
-                data = urlencode({'user_id' : msg['user_id'], 'message' : msg['body'], 'access_token' : key, 'v' : '5.37'})
-                w = urlopen("https://api.vk.com/method/messages.send?" + data)
-            finally:
-                sleep(3)
-
-
-
-
 print('Refresher has started!')
 
 config = configparse('conf.txt')
@@ -47,16 +22,14 @@ frequency = int(config.get('freq', 300))
 
 sesn = Session(config['key'])
 
-refr = Refresher(sesn)
+refr = Refresher(sesn, config)
 
-response = refr.getnewmessages()
-
-update(sesn)
 last_update = time()
 print('Session initialized')
 
 while (True):
     if (last_update + frequency < time()):
         last_update = int(time())
-        update(sesn)
-    sleep(1)
+        refr.update()
+    else:
+        sleep(1)
