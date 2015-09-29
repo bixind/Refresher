71f8ec9723f0cfaee3a3536f8a38ee747bce9eb9
bixind <bixind@users.noreply.github.com>
2015-09-29 22:40:23 +0400

    Ref over!

--- session.py
+++ session.py
@@ -3,6 +3,10 @@ from urllib.parse import *
 from time import *
 import json
 
+def gethashtags(s):
+    return [word for word in s.split() if (word and word[0] == '#' and word is not '#')]
+
+
 class Session:
     last_request = 0
 
@@ -35,9 +39,30 @@ class Refresher:
             self.last_message = response['response']['items'][0]['id']
         return response
 
-    def __init__(self, session):
+    def __init__(self, session, conf):
         self.session = session
         self.last_message = 0
         self.getnewmessages()
+        self.group = conf['group']
 
+    def reply(self, user_id, **kwargs):
+        kwargs['user_id'] = user_id
+        return self.request('messages.send', **kwargs)
 
+    def echo(self, msg, *args):
+        self.reply(msg['user_id'], message = msg['body'])
+
+    def post(self, msg, *args):
+        self.request('wall.post', owner_id = self.group, from_group = 1, message = msg['body'])
+        self.reply(msg['user_id'], message='Posted')
+
+    switch = {'#zapis' : post}
+
+    def update(self):
+        response = self.getnewmessages();
+        for msg in reversed(response['response']['items']):
+            tags = gethashtags(msg['body'])
+            if (tags):
+                self.switch.get(tags[0], self.echo)(self, msg, *tags)
+            else:
+                self.echo(msg)
